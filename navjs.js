8adb64b89e9a531cefadae76fae944b4dd853e8a
dinesh4720 <69290603+dinesh4720@users.noreply.github.com>
2020-11-04 11:59:21 +0530

    Add files via upload

---
+++ navjs.js
@@ -0,0 +1,15 @@
+$(function () {
+ $('.toggle-menu').click(function(){
+    $('.exo-menu').toggleClass('display');
+    $('#pencet').toggleClass('Diam');
+    
+ });
+ 
+});
+
+
+// const clickx= document.getElementById('pencet');
+
+// clickx.addEventListener('click', function(){
+//   clickx.classList.toggle('Diam');
+// });
